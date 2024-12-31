641a4a7996d822b0ba24f2ce5c7cf17e7956fad7
Péter Pallos <pallosp@gmail.com>
2024-12-31 17:57:56 +0100

    Streamline inverseMod and add loose mode

--- src/inverse_mod.ts
+++ src/inverse_mod.ts
@@ -2,27 +2,36 @@
  * Computes a⁻¹ (mod m) with the extended Euclidean algorithm.
  * Returns a number between 0 and m - 1, or NaN if the inverse doesn't exist.
  *
+ * In case strict = false, returns the smallest non-negative solution to the
+ * a·x = gcd(a, m) (mod m) congruence. The result will be in the
+ * [0, m / gcd(a, m) - 1] range.
+ *
  * The inverse of each integer is 0 (mod 1), because a * 0 = 0 ≡ 1 (mod 1).
  */
-export function inverseMod(a: number, m: number): number {
+export function inverseMod(a: number, m: number, strict = true): number {
+  // a and m must be non-negative in order to the result to be correct
   if (m < 0) m = -m;
-  if (m === 1) return 0;
-  a = ((a % m) + m) % m;
-  const m0 = m;
-  let x = 1;
-  let y = 0;
-  while (a > 1) {
-    // q = Math.floor(a / m);
-    const q = (a - a % m) / m;
-    // [m, a] = [a % m, m];
-    let t = m;
-    m = a % m;
-    a = t;
-    // [x, y] = [y, x - q * y];
-    t = y;
-    y = x - q * y;
-    x = t;
+  if (a < 0) a = a % m + m;
+
+  let x = 1, y = 0;
+  while (m !== 0) {
+    const r = a % m;
+    const q = (a - r) / m;
+    const t = x;
+
+    a = m;
+    m = r;
+    x = y;
+    y = t - q * y;
   }
-  if (a === 0) return NaN;
-  return x < 0 ? x + m0 : x;
+
+  // After the loop
+  //   a = gcd(a₀, m₀)
+  //   m = 0
+  //   y = m₀ / gcd(a₀, m₀)
+  //   x = an inverse of a₀ (mod m₀), in the (-y, y) open interval
+  //       x + k·y is also solution for every k ∈ ℤ
+
+  if (a !== 1 && strict) return NaN;
+  return x >= 0 ? x : x + y;
 }
--- tests/inverse_mod.test.ts
+++ tests/inverse_mod.test.ts
@@ -3,6 +3,8 @@ import './lib/matchers';
 import {gcd} from '../src/gcd';
 import {inverseMod} from '../src/inverse_mod';
 
+const MAX_SAFE_PRIME = 9007199254740881;
+
 test('inverseMod', () => {
   // |m| = 1
   expect(inverseMod(0, 1)).toBe(0);
@@ -13,32 +15,77 @@ test('inverseMod', () => {
   expect(inverseMod(0, 2)).toBe(NaN);
   expect(inverseMod(10, 15)).toBe(NaN);
 
+  // gcd(a, m) = 1
   expect(inverseMod(1, 2)).toBe(1);
   expect(inverseMod(-1, 2)).toBe(1);
   expect(inverseMod(7, 15)).toBe(13);
   expect(inverseMod(-7, 10)).toBe(7);
   expect(inverseMod(-7, -10)).toBe(7);
 
+  // https://www.wolframalpha.com/input?i=42x≡1+mod+9007199254740881
+  expect(inverseMod(42, MAX_SAFE_PRIME)).toBe(5361428127821953);
+
   for (let m = 2; m <= 10; m++) {
     for (let i = 0; i < m; i++) {
       if (gcd(m, i) > 1) {
         expect(inverseMod(i, m)).toBe(NaN);
       } else {
-        expect((i * inverseMod(i, m))).toBeCongruent(1, m);
-        expect((i * inverseMod(i, -m))).toBeCongruent(1, m);
-        expect(((m - i) * inverseMod(-i, m))).toBeCongruent(1, m);
+        expect(i * inverseMod(i, m)).toBeCongruent(1, m);
+        expect(i * inverseMod(i, -m)).toBeCongruent(1, m);
+        expect(-i * inverseMod(-i, m)).toBeCongruent(1, m);
       }
+    }
+  }
+});
+
+function inverseModLoose(a: number, m: number): number {
+  return inverseMod(a, m, false);
+}
+
+test('inverseMod, loose mode', () => {
+  // |m| = 1
+  expect(inverseModLoose(0, 1)).toBe(0);
+  expect(inverseModLoose(1, 1)).toBe(0);
+  expect(inverseModLoose(-3, -1)).toBe(0);
+
+  // gcd(a, m) > 1
+  expect(inverseModLoose(0, 2)).toBe(0);
+  expect(inverseModLoose(10, 15)).toBe(2);
+
+  // gcd(a, m) = 1
+  expect(inverseModLoose(1, 2)).toBe(1);
+  expect(inverseModLoose(-1, 2)).toBe(1);
+  expect(inverseModLoose(7, 15)).toBe(13);
+  expect(inverseModLoose(-7, 10)).toBe(7);
+  expect(inverseModLoose(-7, -10)).toBe(7);
+
+  // Big integers
+  expect(42n * BigInt(inverseModLoose(42, MAX_SAFE_PRIME)))
+      .toBeCongruent(1n, BigInt(MAX_SAFE_PRIME));
+  expect(60n * BigInt(inverseModLoose(60, 2 ** 53)))
+      .toBeCongruent(4n, 2n ** 53n);
+
+  for (let m = 2; m <= 10; m++) {
+    for (let a = 0; a < m; a++) {
+      const g = gcd(a, m);
+      const inv = inverseModLoose(a, m);
+      expect(inv).toBeInRange(0, m / g - 1);
+      expect(a * inv).toBeCongruent(g, m);
+      expect(a * inverseModLoose(a, -m)).toBeCongruent(g, m);
+      expect(-a * inverseModLoose(-a, m)).toBeCongruent(g, m);
     }
   }
 });
 
 test('inverseMod benchmark', () => {
   const prime = 23209;
+
   let sum = 0;
-  const start = performance.now();
+  let start = performance.now();
   for (let i = 1; i < prime; i++) sum += inverseMod(i, prime);
   const elapsed = performance.now() - start;
-  console.info(`Computed the inverse of the all residues mod ${prime} in ${
-      elapsed.toFixed(1)} ms`);
   expect(sum).toBe(prime * (prime - 1) / 2);
+
+  console.info(
+      `inverseMod, all residues mod ${prime}: ${elapsed.toFixed(1)} ms`);
 });
