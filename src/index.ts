3673ef51f682e95274ad698988f0d715773395cf
Péter Pallos <pallosp@gmail.com>
2025-01-03 19:39:25 +0100

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export {factor} from './factor';
+export {gcd} from './gcd';
+export {inverseMod} from './inverse_mod';
+export {solveLinearCongruence} from './linear_congruence';
+export {powMod} from './pow_mod';
+export {solveQuadraticCongruence} from './quadratic_congruence';
+export {ALL_RESIDUES, intersectResidues, NO_RESIDUES, ResidueClasses} from './residues';
+export {sqrtModPrime} from './sqrt_mod';
