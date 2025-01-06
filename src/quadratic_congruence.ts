b5bd4832f1de5be4f79ef4a6551d54f625491f99
Péter Pallos <pallosp@gmail.com>
2025-01-06 14:21:17 +0100

    Add implementation comments to the solveQuadraticCongruence* functions.

--- src/quadratic_congruence.ts
+++ src/quadratic_congruence.ts
@@ -33,21 +33,36 @@ export function solveQuadraticCongruenceModPrime(
 
 /**
  * Solves the quadratic congruence equation ax² + bx + c ≡ 0 (mod pᵏ) where p is
- * a prime and k ∈ ℕ. Returns up to two residue classes.
+ * a prime and k ∈ ℕ. Returns up to two residue classes. The modulus of the
+ * return value is a power of p that may be less than pᵏ.
  *
- * The algorithm is based on Hensel lifting expained at
+ * The algorithm is based on Hensel lifting explained at
  * https://sites.millersville.edu/bikenaga/number-theory/prime-power-congruences/prime-power-congruences.html
  */
 export function solveQuadraticCongruenceModPrimePower(
     a: number, b: number, c: number, p: number, k: number): ResidueClasses {
+  // Step 0: handle the k = 0 case
   if (k === 0) return ALL_RESIDUES;
+
+  // Step 1: solve the congruence (mod p)
   const solutionModP = solveQuadraticCongruenceModPrime(a, b, c, p);
   if (k === 1) return solutionModP;
   let {res: r, mod} = solutionModP;
+  if (r.length === 0) return NO_RESIDUES;
   let res = r.concat() as number[];
-  if (res.length === 0) return NO_RESIDUES;
   if (p < 0) p = -p;
+
+  // If all integers are solutions modulo 2, Hensel lifting needs to be done for
+  // x ≡ 0 (mod 2) and x ≡ 1 (mod 2) separately.
   if (mod === 1 && p === 2) [res, mod] = [[0, 1], 2];
+
+  // Step 2
+  //
+  // For each solution x (mod p ** exp)
+  //   While p | 2ax + b, apply the corresponding case of the Hensel lifting
+  //   algorithm to calculate the lifted solution (mod p ** (exp + 1)).
+  //
+  // There is an 1:1 relationship between the original and the lifted solutions.
   let exp = 1;
   let pp = p;
   if (mod > 1) {
@@ -68,6 +83,15 @@ export function solveQuadraticCongruenceModPrimePower(
     mod = pp;
   }
 
+  // Step 3
+  //
+  // From now on p ∤ 2ax + b.
+  //
+  // Apply the corresponding case of the Hensel lifting algorithm case to get
+  // the solutions for the next power of p until reaching pᵏ. While there is an
+  // 1:n relationship between the original and the lifted solutions, the total
+  // number of the latter won't exceed two modulo a suitable power of p. It's
+  // enough to find 3 solutions to determine the new modulus.
   for (; exp < k; exp++) {
     const newRes: number[] = [];
     const nextPp = pp * p;
@@ -80,6 +104,9 @@ export function solveQuadraticCongruenceModPrimePower(
       }
     }
     res = newRes;
+
+    // Normalize the result. Find the appropriate modulus for which there are at
+    // most 2 residues.
     if (res.length === 0) return NO_RESIDUES;
     if (res.length === 2 && pp === 2) {
       mod = 1;
@@ -104,19 +131,23 @@ export function solveQuadraticCongruenceModPrimePower(
  *
  * The modulus can be specified either as a number or a list of monotonously
  * growing positive prime factors.
+ *
+ * Number of results ≤ 2 ** (number of distinct prime factors).
  */
 export function solveQuadraticCongruence(
     a: number, b: number, c: number, m: number|number[]): ResidueClasses {
   const modFactors = Array.isArray(m) ? m : factor(m);
-  if (modFactors.length === 0) return ALL_RESIDUES;
   let res = [0];
   let mod = 1;
+  // Step 1: Split the modulus to the product of prime factors.
   let p = modFactors[0];
   let k = 1;
   for (let i = 1; i <= modFactors.length; i++) {
     if (modFactors[i] !== p) {
+      // Step 2: Solve the congruence (mod pᵏ)
       const {res: fRes, mod: fMod} =
           solveQuadraticCongruenceModPrimePower(a, b, c, p, k);
+      // Step 3: Combine the solutions using the Chinese remainder theorem.
       if (fRes.length === 0) return NO_RESIDUES;
       if (fMod > 1) {
         const coeff = (inverseMod(mod, fMod) + fMod) * mod;
