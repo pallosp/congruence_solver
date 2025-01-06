1797df28e73bd69a2c1c71b8316511753e47af52
Péter Pallos <pallosp@gmail.com>
2025-01-06 12:41:13 +0100

    Fix ResidueClasses re-exporting

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export {inverseMod} from './inverse_mod';
 export {solveLinearCongruence} from './linear_congruence';
 export {powMod} from './pow_mod';
 export {solveQuadraticCongruence} from './quadratic_congruence';
-export {ALL_RESIDUES, intersectResidues, NO_RESIDUES, ResidueClasses} from './residues';
+export {ALL_RESIDUES, intersectResidues, NO_RESIDUES} from './residues';
+export type {ResidueClasses} from './residues';
 export {sqrtModPrime} from './sqrt_mod';
