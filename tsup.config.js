d2d75f3e38d6b20b041c4074335b23db2663fd22
Péter Pallos <pallosp@gmail.com>
2025-01-30 12:59:34 +0100

    Dedupe the generated .d.ts files

--- tsup.config.js
+++ tsup.config.js
@@ -2,9 +2,7 @@ import {defineConfig} from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
+  minify: true,
   splitting: false,
   sourcemap: true,
-  clean: true,
 });
